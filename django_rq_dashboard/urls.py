3c58115656e2df5b2ea8d28032d54eae262df369
Bruno Renié <brutasse@gmail.com>
2015-07-10 11:43:27 +0200

    Remove use of patterns()

--- django_rq_dashboard/urls.py
+++ django_rq_dashboard/urls.py
@@ -1,12 +1,12 @@
-from django.conf.urls import url, patterns
+from django.conf.urls import url
 
 from . import views
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', views.stats, name='rq_stats'),
     url(r'^queues/(?P<queue>.+)/$', views.queue, name='rq_queue'),
     url(r'^workers/(?P<worker>.+)/$', views.worker, name='rq_worker'),
     url(r'^jobs/(?P<job>.+)/$', views.job, name='rq_job'),
     url(r'^scheduler/(?P<queue>.+)/$', views.scheduler, name='rq_scheduler'),
-)
+]
