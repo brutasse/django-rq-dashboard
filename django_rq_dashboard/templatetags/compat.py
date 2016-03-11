c1b7f4431466dd4891924c0b3422e77ea23e91c7
olarin <oleg.larin@gmail.com>
2016-03-11 12:13:20 +0300

    typo fixed

--- django_rq_dashboard/templatetags/compat.py
+++ django_rq_dashboard/templatetags/compat.py
@@ -1,6 +1,6 @@
 from django.template import Library
 
-try:  # Django < 1.5
+try:  # Django < 1.9
     from django.templates.future import url as url_compat
 except ImportError:
     from django.template.defaulttags import url as url_compat
