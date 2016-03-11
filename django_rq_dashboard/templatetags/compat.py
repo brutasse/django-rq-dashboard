b714513d28849c94be33d8977869691cc74edf8b
Oleg Larin <oleg.larin@gmail.com>
2016-03-11 11:57:38 +0300

    Django 1.9 compatibility

---
+++ django_rq_dashboard/templatetags/compat.py
@@ -0,0 +1,14 @@
+from django.template import Library
+
+try:  # Django < 1.5
+    from django.templates.future import url as url_compat
+except ImportError:
+    from django.template.defaulttags import url as url_compat
+
+
+register = Library()
+
+
+@register.tag
+def url(parser, token):
+    return url_compat(parser, token)
